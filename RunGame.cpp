f6bb843e3a6eaf2bf5ba90892af8b6df00166ada
BarraSantiago <santiago.barra@ing.austral.edu.ar>
2022-09-20 09:37:16 -0300

    Triangle Movement

--- RunGame.cpp
+++ RunGame.cpp
@@ -7,36 +7,58 @@ void RunGame()
 	int width = 720;
 	int height = 480;
 	InitWindow(width, height, "Asteroids");
-
-
+	Vector2 mousePos = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+	Player player = InitPlayer();
 	while (!WindowShouldClose())
 	{
-		DrawGame();
+		DrawGame(player);
+		if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+			mousePos = GetMousePosition();
+		if (player.front.x > mousePos.x)
+			player.front.x -= 600.0f * GetFrameTime();
+		else if (player.front.x < mousePos.x)
+			player.front.x += 600.0f * GetFrameTime();
+
+		if (player.front.y > mousePos.y)
+			player.front.y -= 600.0f * GetFrameTime();
+		else if (player.front.y < mousePos.y)
+			player.front.y += 600.0f * GetFrameTime();
 	}
 
 	CloseWindow();
 
 }
 
-void DrawGame()
+void ReposPlayer(Player player)
+{
+	Vector2 mousePos = GetMousePosition();
+	Vector2 direcVector = mousePos - player.front;
+	float angle = 
+}
+
+void DrawGame(Player player)
 {
 	BeginDrawing();
 	ClearBackground(BLACK);
-	DrawPlayer();
+
+	DrawPlayer(player);
 	EndDrawing();
 }
 
-void DrawPlayer() 
+void DrawPlayer(Player player) 
 {
 	const Color NEONCYAN = CLITERAL(Color) { 4, 217, 255, 255 };
-	Vector2 a, b, c;
-	a = GetMousePosition();
-	b = { a.x-50, a.y-50 };
-	c = { a.x+50, a.y+50 };
+	player.right = { player.front.x + 50, player.front.y + 50 };
+	player.left = { player.front.x - 50, player.front.y + 50 };
+	//DrawText(TextFormat("c.y: %i", (int)c.y),GetScreenWidth() / 2,GetScreenHeight() /   2 + 150, GetScreenHeight()/13, WHITE);
+	DrawTriangle(player.front, player.left, player.right, NEONCYAN);
+}
 
-	DrawText(TextFormat("b.x: %i", (int)b.x), GetScreenWidth() / 2, GetScreenHeight() / 2, GetScreenHeight() / 13, WHITE);
-	DrawText(TextFormat("b.y: %i", (int)b.y), GetScreenWidth() / 2, GetScreenHeight() / 2 + 50, GetScreenHeight() / 13, WHITE);
-	DrawText(TextFormat("c.x: %i", (int)c.x), GetScreenWidth() / 2, GetScreenHeight() / 2 + 100, GetScreenHeight() / 13, WHITE);
-	DrawText(TextFormat("c.y: %i", (int)c.y),GetScreenWidth() / 2,GetScreenHeight() /   2 + 150, GetScreenHeight()/13, WHITE);
-	DrawTriangle(a,b,c, NEONCYAN);
+Player InitPlayer()
+{
+	Player player;
+	player.front = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+	player.right = { player.front.x + 50, player.front.y + 50 };
+	player.left = { player.front.x - 50, player.front.y + 50 };
+	return player;
 }
--- RunGame.h
+++ RunGame.h
@@ -3,6 +3,14 @@
 #include <string>
 #include "raylib.h"
 
+struct Player
+{
+	Vector2 front;
+	Vector2 left;
+	Vector2 right;
+};
+
 void RunGame();
-void DrawGame();
-void DrawPlayer();
+void DrawGame(Player player);
+void DrawPlayer(Player player);
+Player InitPlayer();
